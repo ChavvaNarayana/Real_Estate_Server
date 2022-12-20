dd5f2db0e49c149ab1ac354ee5c43d0dab2e5668
rohityellapu <rohithyellapu24@gmail.com>
2022-12-20 17:16:31 +0530

    added routing

---
+++ routes/login.js
@@ -0,0 +1,13 @@
+const router = require('express').Router()
+
+router.post('/login', (req, res) => {
+    res.send('login ')
+})
+
+router.post('/register', (req, res) => {
+    res.send('register ')
+})
+
+
+
+module.exports = router;
