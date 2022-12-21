38d1030b9cc576da90ebfec0994752d5df78ca04
Chavva <narayanachavva@gail.com>
2022-12-21 10:30:55 +0530

    added more models by chavva

--- models/property.js
+++ models/property.js
@@ -3,41 +3,53 @@ const mongoose = require('mongoose')
 const { Schema } = mongoose;
 
 const propertySchema = new Schema({
-    PPID: {
-        type: String,
-        unique: true
-    },
-    basiInfo: {
-        propertyType: {
-            required: true,
-            type: String,
-        },
-        propertyAge: {
-            type: Number,
-            required: true,
-        }
-    },
-    generalInfo: {
-        mobile: {
-            required: true,
-            type: Number,
-        },
-    },
-    locationInfo: {
-        lat: {
-            type: Number
-        },
-        long: {
-            type: Number
-        }
-    },
-    propertyDetail: {
-        area: {
-            required: true,
-            type: Number,
-        },
-    }
+    
+    //added more models by chavva
+    propertyType: { required: true, type: String },
+    negotiable: { type: String },
+    price: { type: String },
+    ownership: { type: String },
+    propertyAge: { type: String },
+    propertyApproved: { type: String },
+    propertyAge: { type: String },
+    propertyDescription: { type: String },
+    bankLoan: { type: String },
+    length: { required: true, type: String },
+    breath: { required: true, type: String },
+    totalArea: { required: true, type: Number },
+    areaUnit: { type: String },
+    noOfBHK: { type: String },
+    noOfFloor: { type: String },
+    attached: { type: String },
+    western: { type: String },
+    furnished: { type: String },
+    carParking: { type: String },
+    lift: { type: String },
+    electricity: { type: String },
+    facing: { type: String },
 
+    name: { type: String },
+    mobile: { required: true, type: String },
+    postedBy: { type: String },
+    saleType: { type: String },
+    featuredPackage: { type: String },
+    ppdPackage: { type: String },
+
+    email: { required: true, type: String },
+    city: { type: String },
+    area: { type: String },
+    pincode: { type: String },
+    address: { type: String },
+    landmark: { type: String },
+    latitude: { type: String },
+    longitude: { type: String },
+    PPDId: { required: true, type: String },
+    Views: { required: true, type: String },
+    DaysLeft: { required: true, type: String },
+
+    image: { type: String },
+    status: { type: String, default: "Unsold" },
+    userid: { type: String },
 
 })
 
