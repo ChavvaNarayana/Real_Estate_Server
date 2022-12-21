48f64cf9dea2d6164c9eb2845283e2d6c3423f38
rohityellapu <rohithyellapu24@gmail.com>
2022-12-21 11:33:18 +0530

    debugged login and register routes

--- routes/register.js
+++ routes/register.js
@@ -9,45 +9,48 @@ const bcrypt = require("bcrypt");
 
 
 router.post('/', async (req, res) => {
-    const { username, email, password } = req.body;
-   if (!username || !email || !password) {
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) {
     return res.status(400).json({ msg: "Please enter all fields" });
   }
-   await User.find({ email }).then(user => {
-    if (user) return res.status(400).json({ msg: "User already exists" });
-    const newUser = new User({
-      username,
-      email,
-      password
-    });
-     bcrypt.genSalt(10, (err, salt) => {
-      bcrypt.hash(newUser.password, salt,async (err, hash) => {
+  console.log(username, email, password)
+  await User.find({ mail: email }).then((user) => {
+    console.log(user);
+    if (user.length > 0) return res.status(400).json({ msg: "User already exists" });
+  })
+  let ppid = 'PPD'
+  const newUser = new User({
+    name:username,
+    mail: email,
+    password,
+    ppid
+    
+  });
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(newUser.password, salt, async (err, hash) => {
+      if (err) throw err;
+      newUser.password = hash;
+      await newUser.save()
+      jwt.sign({ id: newUser.id }, process.env.JWT_SECRET, { expiresIn: 3600 }, (err, token) => {
         if (err) throw err;
-        newUser.password = hash;
-       await newUser.save().then(user => {
-          jwt.sign(
-            { id: user.id },
-            config.get("jwtSecret"),
-            { expiresIn: 3600 },
-            (err, token) => {
-              if (err) throw err;
-              res.json({
-                token,
-                user: {
-                  id: user.id,
-                  username: user.username,
-                  email: user.email
-                }
-              });
-            }
-          );
+        res.json({
+          token,
+          user: {
+            id: newUser.id,
+            username: newUser.name,
+            email: newUser.mail,
+            ppid
+          }
         });
-      });
+      }
+      );
+
     });
   });
+
 })
 
-  
+
 
 
 
