d4ee53019a13db276b53d64de303446c6d8b63d4
rohityellapu <rohithyellapu24@gmail.com>
2022-12-22 13:59:10 +0530

    modified structure of code

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,20 @@
 const router = require('express').Router()
 const User = require('../models/user')
-router.get('/',async  (req, res) => {
-    const { email, ppdId, token, username } = req.body;
-    // if (!email || !ppdId || !token || !username) {
-    //     res.status(400).json({
-    //         status: "Please log in or sign up"
-    //     })
-    // }
-    try {
 
-        const userProperties = await User.findOne({ mail: email }).populate('properties').sort({createdAt: -1});
+router.get('/', async (req, res) => {
+
+    try {
+        const userProperties = await User.findOne({ mail: req.headers.email }).populate('properties').sort({ createdAt: -1 });
         res.json({
             status: "Sucess",
             properties: userProperties.properties,
-            
         })
     } catch (err) {
-        res.status(400).json({
+        res.status(404).json({
             status: "Error",
             message: err.message
         })
     }
-
 })
 
 module.exports = router;
